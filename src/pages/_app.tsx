6897e97c2f376ddb7478eefa43a761ee362c8ec6
Amir Razin <amir.alrazin@gmail.com>
2021-06-27 10:25:54 +0600

    initial commit | Bismillah

---
+++ src/pages/_app.tsx
@@ -0,0 +1,36 @@
+import { ChakraProvider, Img } from "@chakra-ui/react";
+import { AppProps } from "next/app";
+import Head from "next/head";
+import "@fontsource/lexend/latin.css";
+
+import Layout from "components/layout";
+
+import customTheme from "styles/customTheme";
+import "styles/globals.css";
+
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  return (
+    <ChakraProvider  theme={customTheme}>
+      <Head>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover"
+        />
+      </Head>
+      <Img
+        position="fixed"
+        backgroundSize="cover"
+        src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg"
+        h="100%"
+        w="100%"
+        filter="blur(2px)"
+        zIndex="-1"
+      />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ChakraProvider>
+  );
+};
+
+export default MyApp;
---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import { Box } from "@chakra-ui/layout";
+
+const Home = () => {
+  return (
+    <Box mb={8} w="full">
+    </Box>
+  );
+};
+
+export default Home;
